217d1f83bdd619d4cbaeb7d65b27897cc7e4a90b
zullum <zullumsan@gmail.com>
2022-02-21 17:04:17 +0100

    Adding useState example.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import pokemon from "./pokemon.json";
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+const PokemonRow = ({ pokemon, onSelect }) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(", ")}</td>
+    <td>
+      <button onClick={() => onSelect(pokemon)}>Select!</button>
+    </td>
+  </tr>
+);
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string.isRequired,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string.isRequired),
+  }),
+  onSelect: PropTypes.func,
+};
+
+const PokemonInfo = ({ name, base }) => (
+  <div>
+    <h1>{name.english}</h1>
+    <table>
+      <tbody>
+        {Object.keys(base).map((key) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{base[key]}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
+
+PokemonInfo.propTypes = {
+  name: PropTypes.shape({
+    english: PropTypes.string,
+  }),
+  base: PropTypes.shape({
+    HP: PropTypes.number.isRequired,
+    Attack: PropTypes.number.isRequired,
+    Defense: PropTypes.number.isRequired,
+    "Sp. Attack": PropTypes.number.isRequired,
+    "Sp. Defense": PropTypes.number.isRequired,
+    Speed: PropTypes.number.isRequired,
+  }),
+};
 
 function App() {
+  const [filter, setFilter] = useState("");
+  const [selectedItem, setSelectedItem] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1rem",
+      }}
+    >
+      <h1 className="title">Pokemon Search</h1>
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "70% 30%",
+          gridColumnGap: "1rem",
+        }}
+      >
+        <div>
+          <input
+            type="text"
+            value={filter}
+            onChange={(evt) => setFilter(evt.target.value)}
+          />
+          <table width="100%">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon
+                .filter((pokemon) =>
+                  pokemon.name.english
+                    .toLowerCase()
+                    .includes(filter.toLowerCase())
+                )
+                .slice(0, 20)
+                .map((pokemon) => (
+                  <PokemonRow
+                    pokemon={pokemon}
+                    key={pokemon.id}
+                    onSelect={() => setSelectedItem(pokemon)}
+                  />
+                ))}
+            </tbody>
+          </table>
+        </div>
+
+        {selectedItem && <PokemonInfo {...selectedItem} />}
+      </div>
     </div>
   );
 }
